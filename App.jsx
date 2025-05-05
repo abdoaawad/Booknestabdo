19e0649269437e94e779569c9024075591816f44
abdoaawad <a1b9d123456@gmail.com>
2025-05-06 00:03:16 +0300

    Booknest

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Mindtales</h1>
+      <p>Your go-to place for books, stories, and personal experiences.</p>
+    </div>
+  );
+}
+
+export default App;
